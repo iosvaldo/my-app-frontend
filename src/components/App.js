63f56b620adb59032cb3f7e323c72aa13b90d22c
osvaldo <iosvaldo.app@gmail.com>
2022-06-21 15:12:21 -0400

    megre from norah

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import '../App.css';
+import Services from './Services';
 import Home   from './Home'
 import Navigate from './Navigate';
 import Appointments from './Appointments'
-import Services from './Services';
-import AppointmentList from './AppointmentList';
-// import ‘bootstrap/dist/css/bootstrap.min.css’;
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 
 function App() {
   return (
-    <div className="App">
-      <Navigate />
-      <Appointments />
-      <Services />
-  
-    </div>
+    <Router>
+      <div className="App">
+        <Navigate className="App-header"/>
+        <header >
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/appointments">
+                <Appointments />
+              </Route>
+              <Route exact path="/form">
+                <Appointments />
+              </Route>
+              <Route exact path="/services">
+                <Services />
+              </Route>
+            </Switch>
+        </header>
+      </div>
+    </Router>
+     
   );
 }
 
