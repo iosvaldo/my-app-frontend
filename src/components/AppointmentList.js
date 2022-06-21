09c3ae03407b5ce7844576cafe8f7809cd662dd4
[Norah Taylor] <emt368@gmail.com>
2022-06-21 12:18:24 -0400

    fetching

--- src/components/AppointmentList.js
+++ src/components/AppointmentList.js
@@ -1,11 +1,24 @@
 import React, {useState, useEffect} from 'react'
+import Card from 'react-bootstrap/Card'
 
-function AppointmentList() {
+function AppointmentList({appointmentType, name, date}) {
 
 
 
   return (
-    <div>AppointmentList</div>
+    <div>
+      <h1>Testing</h1>
+      <Card className="text-center">
+      <Card.Header>Your Appointment</Card.Header>
+        <Card.Body>
+        <Card.Title>{name}</Card.Title>
+          <Card.Text>{date}</Card.Text>
+          <Card.Text></Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+        </Card.Body>
+      <Card.Footer className="text-muted">{appointmentType}</Card.Footer>
+    </Card>
+    </div>
   )
 }
 
